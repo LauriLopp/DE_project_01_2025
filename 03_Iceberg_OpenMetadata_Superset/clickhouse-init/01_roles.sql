a199b3b532b0944aedf9e7c8b26204ee71b25488
LauriLopp <lopplauri@gmail.com>
2025-11-24 19:18:30 +0200

    Moved roles and users creation to clickhouse-init; made init more granular and fixed bugi.

--- 03_Iceberg_OpenMetadata_Superset/clickhouse-init/01_roles.sql
+++ 03_Iceberg_OpenMetadata_Superset/clickhouse-init/01_roles.sql
@@ -1,3 +1,6 @@
+-- Role for the pipeline
+CREATE ROLE IF NOT EXISTS project_rw;
+
 -- Create roles for analysts
 CREATE ROLE IF NOT EXISTS analyst_full;
 CREATE ROLE IF NOT EXISTS analyst_limited;
--- 03_Iceberg_OpenMetadata_Superset/sql/02_create_users.sql
+++
@@ -1,5 +0,0 @@
--- Create users for analysts
-CREATE USER IF NOT EXISTS user_full
-    IDENTIFIED BY 'strong_password_full';
-CREATE USER IF NOT EXISTS user_limited 
-    IDENTIFIED BY 'strong_password_limited';
